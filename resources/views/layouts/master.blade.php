20d6d8866987496b472597f563d07dd643995db8
Administrator <admin@local.host>
2015-04-03 12:28:41 +0300

    init

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"/>
+	<link rel="stylesheet" href="{{asset('css/styles.css')}}"/>
+	<script src="{{asset('js/jquery.min.js')}}"></script>
+	<script src="{{asset('js/bootstrap.min.js')}}"></script>
+</head>
+<body>
+<div class="container">
+	@yield('content')
+</div>
+</body>
+</html>
